fa37694019f1b9a3b056109b66f22f579c1eaf2e
elainplva <s00250500@atu.ie>
2025-01-28 15:42:52 +0000

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'kptest';
+  title = 'To Do';
+
+  thingsTODo = [
+    "Learn Javascript",
+    "Learn Angular",
+    "Learn Redux"
+  ]
 }
+
